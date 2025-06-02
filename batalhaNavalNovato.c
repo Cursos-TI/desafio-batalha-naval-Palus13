ecb0480327e67c49577c7b8417836af24375a11c
IghorRibeiro <ighoribeiropb@gmail.com>
2025-06-02 18:50:26 -0300

    Update batalhaNavalNovato.c
    Realizado desafio Novato, posicionando os navios em uma posição predeterminada e transcrevendo o tabuleiro com os navios

--- batalhaNavalNovato.c
+++ batalhaNavalNovato.c
@@ -10,31 +10,30 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int tabuleiro[10][10] = {0}; // Inicializando o tabuleiro só com água (0)
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // Posicionando navios
+    // Navio 1 - Horizontal
+    for (int i = 5; i<= 7; i++) {
+        tabuleiro [3][i] = 3;
+    }
+    // Navio 2 - Vertical
+    for (int i=3;i<=5;i++) {
+        tabuleiro[i][8] = 3;
+    }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    char letra = 'A';
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    // Exibindo o tabuleiro com os navios posicionados
+    printf("TABULEIRO DE BATALHA NAVAL\n");
+    printf("  0  1  2  3  4  5  6  7  8  9\n");
+    for (int i= 0; i<=9;i++) {
+        printf ("%c ",letra + i);
+        for (int j=0; j<=9;j++) {
+            printf("%d ", tabuleiro[i][j]);
+            printf(" ");
+        }
+        printf("\n");
+    }
     return 0;
 }
